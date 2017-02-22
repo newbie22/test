9fa71417c0e117a9b16bfbc6a391df9746b0bacb
newbie22 <vcameron92000@yahoo.com>
2017-02-22 17:48:37 -0500

    Add files via upload

---
+++ GeometricObject.java
@@ -0,0 +1,21 @@
+
+
+public class GeometricObject {
+  private String color = "white";
+  private boolean filled;
+  private java.util.Date dateCreated;
+  
+  /** Construct a default geometric object */
+  public GeometricObject() {
+    dateCreated = new java.util.Date();
+  }
+
+  /** Construct a geometric object with the specified color 
+    *  and filled value */
+  public GeometricObject(String color, boolean filled) {
+    dateCreated = new java.util.Date();
+    this.color = color;
+    this.filled = filled;
+  }
+
+}
